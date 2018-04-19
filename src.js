6ba47fd10af82f08122ef4445b894204a6ac82a2
Akihiro Yamamoto <fkmks247@gmail.com>
2018-04-19 15:38:31 +0900

    Update src.js

--- src.js
+++ src.js
@@ -1,59 +1,82 @@
 const EVENT_SUBMITTED = 'submitted'
+const EVENT_TASK_DELETED = 'deleted'
 
 // the Root View (controller view)
 const ParentView = Backbone.View.extend({
-	initialize: () => {
-		// create models and collections
-		this.taskCollection = new TaskCollection()
+  initialize: () => {
+    // create models and collections
+    this.taskCollection = new TaskCollection()
 
-		// create sub views
-		this.tasksView = new TasksView({ collection: this.taskCollection })
-		this.addTaskView = new AddTaskView()
+    // create sub views
+    this.tasksView = new TasksView({ collection: this.taskCollection })
+    this.addTaskView = new AddTaskView()
 
-		// set event handler
-		this.addTaskView.on(EVENT_SUBMITTED, (value) => {
-			const model = new TaskModel({
-				title: value
-			})
-			this.taskCollection.add(model)
-		})
-	},
+    // set event handlers
+    this.addTaskView.on(EVENT_SUBMITTED, (value) => {
+      const model = new TaskModel({
+        title: value
+      })
+      this.taskCollection.add(model)
+    })
+  },
 })
 
 
 // this has input form (presentational view)
 const AddTaskView = Backbone.View.extend({
-	events: {
-		submit: 'onSubmitted'
-	},
+  el: '.....',
 
-	onSubmitted: (e) => {
-		this.trigger(EVENT_SUBMITTED, $('#title').val())
-	},
+  events: {
+    submit: 'onSubmitted'
+  },
+
+  onSubmitted: (e) => {
+    this.trigger(EVENT_SUBMITTED, $('#title').val())
+  },
 })
 
 
 // mixed presentational and controller
 const TasksView = Backbone.View.extend({
-	el: '.....',
+  el: '.....',
 
-	// NOTE: this class does NOT have any "taskViewList" or something,
-	// because this class has collection, collection includes models,
-	// and the each model is attached to the view, so we can access all views
-	initialize: () => {
-		this.collection.on('add', (model) => {
-			const newView = new TaskView(model)
-			this.$el.append(this.newView.$el);
-			newView.render()
-		})
-	},
+  // NOTE: this class does NOT have any "taskViewList" or something,
+  // because this class has collection, collection includes models,
+  // and the each model is attached to the view, so we can access all views
+  initialize: () => {
+    this.collection.on('add', (model) => {
+      const newView = new TaskView(model)
+
+      // set event handlers
+      newView.once(EVENT_TASK_DELETED, () => {
+        newView.remove()
+        this.collection.remove(model)
+      })
+
+      // render
+      this.$el.append(this.newView.$el);
+      newView.render()
+    })
+  },
 })
 
 
 // single task view (presentational view)
-const TaskView = Backbone.View.extend(/*TODO*/)
+const TaskView = Backbone.View.extend({
+  el: '.....',
 
-// single task model
-const TaskModel = Backbone.Model.extend(/*TODO*/)
+  events: {
+    'click .delete': 'onDeleteClicked'
+  },
 
+  onDeleteClicked: (e) => {
+    this.trigger(EVENT_TASK_DELETED)
+  },
+
+  render: () => { /*TODO*/ },
+})
+
+
+// collection and model
 const TaskCollection = Backbone.Collection.extend({ model: TaskModel })
+const TaskModel = Backbone.Model.extend(/*TODO*/)
